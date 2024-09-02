e3d809b725f262e8bd6dbc7d7c2cd1328c2cdeee
Ali Aliev <ali@avatarify.ai>
2024-09-02 10:35:17 -0500

    add nvm

--- common-dev/start.sh
+++ common-dev/start.sh
@@ -2,4 +2,4 @@
 
 /usr/sbin/sshd -D &
 
-runuser -l user -c "jupyter-lab --ip=0.0.0.0 --port=8555 --no-browser --notebook-dir=/home/user/sources"
+runuser -l user -c "jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user/sources"
