ffc2690652eae678ef83ce1d7370cf134ca6086b
Ali Aliev <ali@avatarify.ai>
2024-10-31 09:00:45 -0500

    run start.sh as user

--- common-dev/start.sh
+++ common-dev/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/sbin/sshd -D &
+sudo /usr/sbin/sshd -D &
 
-runuser -l user -c "jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user/sources"
+jupyter-lab --ip=0.0.0.0 --port=8500 --no-browser --notebook-dir=/home/user/sources
